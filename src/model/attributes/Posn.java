4e1452d8f8682675571682451bbaeaad51b3c2f7
zhaoChar <zhao.char@northeastern.edu>
2022-03-23 08:34:51 -0400

    bug fixes

--- src/model/attributes/Posn.java
+++ src/model/attributes/Posn.java
@@ -81,7 +81,7 @@ public class Posn {
     }
     if (other instanceof Posn) {
       Posn a = (Posn) other;
-      return this.x - a.x < 0.001 && this.y - a.y < 0.001;
+      return Math.abs(this.x - a.x) < 0.001 && Math.abs(this.y - a.y) < 0.001;
     } else {
       return false;
     }
