c3801cc6a84584db587f3f1f8664498906b2c03b
zhaoChar <zhao.char@northeastern.edu>
2022-03-23 16:40:21 -0400

    made class abstract and removed functionality

--- src/controller/commands/ResizeCmd.java
+++ src/controller/commands/ResizeCmd.java
@@ -6,80 +6,23 @@ import model.ObjectInterfaces.Scalable;
 
 
 /**
- * Command that represents resizing an object over time.
+ * Represents common attributes for a command that changes an object's dimensions over time.
  */
-public class ResizeCmd extends GradualCmd {
-
-  private Integer x;
-  private Integer y;
-  private int destX;
-  private int destY;
-  private String log;
-  private double dx;
-  private double dy;
-
+public abstract class ResizeCmd extends GradualCmd {
+  protected double scale;
 
   /**
-   * Constructor for GradualCmd.
+   * Constructor for ResizeCmd.
    *
    * @param obj   the object this command functions on.
    * @param start the tick when this command triggers.
    * @param end   the tick when this command ends.
    */
-  public ResizeCmd(Drawable obj, int start, int end, int destX, int destY) {
+  public ResizeCmd(Drawable obj, int start, int end, double scale) {
     super(obj, start, end);
     if (!(obj instanceof Scalable)) {
       throw new IllegalArgumentException("Error: This object is not instance of Scalable");
     }
-    this.destX = destX;
-    this.destY = destY;
-  }
-
-  @Override
-  public void execute() {
-    super.execute();
-    if (x == null || y == null) {
-      x = (((Scalable) obj).getXPos());
-      y = (((Scalable) obj).getYPos());
-      this.log = obj.getName() + " moves from : (" + x + ", " + y + ")" + " to " + "(" + destX +
-          ", " + destY + ")" + " from t=" + (startTick - 1) + " to t=" + endTick;
-      dx = (destX - x) / (endTick - startTick + 1);
-      dy = (destY - y) / (endTick - startTick + 1);
-      if (dx > 0) {
-        ((Scalable) obj).stretchHorizontal(dx);
-        if (dy > 0) {
-          ((Scalable) obj).stretchVertical(dy);
-        }
-        if (dy < 0) {
-          ((Scalable) obj).shrinkVertical(dy);
-        }
-      }
-      if (dx < 0) {
-        ((Scalable) obj).shrinkHorizontal(dx);
-        if (dy > 0) {
-          ((Scalable) obj).stretchVertical(dy);
-        }
-        if (dy < 0) {
-          ((Scalable) obj).shrinkVertical(dy);
-        }
-      }
-      if (dx == 0) {
-        if (dy > 0) {
-          ((Scalable) obj).stretchVertical(dy);
-        }
-        if (dy < 0) {
-          ((Scalable) obj).shrinkVertical(dy);
-        }
-      }
-    }
-
-  }
-
-  @Override
-  public String logCmd() {
-    if (!running && !complete) {
-      throw new IllegalStateException("Error: command has not run");
-    }
-    return this.log;
+    this.scale = scale;
   }
 }
