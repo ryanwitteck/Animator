88cbac831ce72a308416880d1f6913622a46db6f
zhaoChar <zhao.char@northeastern.edu>
2022-03-31 13:28:18 -0400

    documentation

--- src/model/shapes/IShape.java
+++ src/model/shapes/IShape.java
@@ -6,6 +6,7 @@ import model.interfaces.Scalable;
 
 /**
  * Our interface for various shapes that we want to create.
+ * All shapes extend the Movable, Drawable, and Scalable interfaces.
  * Subject to change.
  */
 public interface IShape extends Movable, Drawable, Scalable {
