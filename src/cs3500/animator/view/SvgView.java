f6512917e45960181d804bf4777f5521dbf6d3a1
zhaoChar <zhao.char@northeastern.edu>
2022-04-03 11:53:17 -0400

    started SvgView class

---
+++ src/cs3500/animator/view/SvgView.java
@@ -0,0 +1,12 @@
+package cs3500.animator.view;
+
+import java.io.IOException;
+
+public class SvgView implements AnimationView {
+
+
+  @Override
+  public void renderAnimation() throws IOException {
+
+  }
+}
