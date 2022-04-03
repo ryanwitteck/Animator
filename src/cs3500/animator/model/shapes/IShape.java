82c17aa316458f5a1101ee9475ae2cdb75e5be93
zhaoChar <zhao.char@northeastern.edu>
2022-04-03 01:28:39 -0400

    Documentation

--- src/cs3500/animator/model/shapes/IShape.java
+++ src/cs3500/animator/model/shapes/IShape.java
@@ -6,7 +6,7 @@ import cs3500.animator.model.interfaces.Scalable;
 
 /**
  * Our interface for various shapes that we want to create.
- * All shapes extend the Movable, Drawable, and Scalable interfaces.
+ * IShape extends the Movable, Drawable, and Scalable interfaces.
  * Subject to change.
  */
 public interface IShape extends Movable, Drawable, Scalable {
