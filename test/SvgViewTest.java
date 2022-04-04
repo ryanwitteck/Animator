c2392659699a25bbacd2c8f301821a581f2dba29
zhaoChar <zhao.char@northeastern.edu>
2022-04-04 03:38:09 -0400

    more testing

--- test/SvgViewTest.java
+++ test/SvgViewTest.java
@@ -11,6 +11,7 @@ import cs3500.animator.model.attributes.Color;
 import cs3500.animator.model.attributes.Posn;
 import cs3500.animator.model.commands.AddOvalCmd;
 import cs3500.animator.model.commands.AddRectCmd;
+import cs3500.animator.model.commands.ChangeColorCmd;
 import cs3500.animator.model.commands.ICommand;
 import cs3500.animator.model.commands.MoveCmd;
 import cs3500.animator.model.commands.PlaceCmd;
@@ -60,6 +61,7 @@ public class SvgViewTest {
     a.addCmd(new AddOvalCmd("O", 100, 100, 40, 40, new Color(90, 10, 70), 2));
     a.addCmd(new MoveCmd("R", 1, 10, new Posn(100, 100), new Posn(0, 0)));
     a.addCmd(new MoveCmd("O", 5, 15, new Posn(100, 100), new Posn(200, 200)));
+    a.addCmd(new ChangeColorCmd("O", 5, 15, new Color(90, 10, 70), new Color(40, 200, 150)));
     a.compile();
     AnimationView view = new SvgView(a, builder, 3);
     view.renderAnimation();
