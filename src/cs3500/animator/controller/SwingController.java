9fc6d566d2b8d97741a93fc803b13df9d4bc524e
zhaoChar <zhao.char@northeastern.edu>
2022-04-11 23:48:14 -0400

    changed imports from .* to the specifics

--- src/cs3500/animator/controller/SwingController.java
+++ src/cs3500/animator/controller/SwingController.java
@@ -1,13 +1,19 @@
 package cs3500.animator.controller;
 
-import java.awt.*;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.IOException;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
 
 import cs3500.animator.view.InteractiveView;
 
